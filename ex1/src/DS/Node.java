207538d1181cc883da274eb6640c4f5175c89cc0
solanoam <noam5456@gmail.com>
2018-11-14 13:13:20 +0200

    Adding Ronen as auther

--- ex1/src/DS/Node.java
+++ ex1/src/DS/Node.java
@@ -1,6 +1,6 @@
 /**************************************
- * Author: Noam Solan
- * ID: 204484703
+ * Authors: Noam Solan & Ronen Rozin
+ * ID: 204484703 (Noam) & 318257011 (Ronen)
  * Course: 83-459-01 (Software engineering)
  * Practice: 83-459-03 (Gil)
  **************************************/
@@ -43,4 +43,3 @@ public class Node {
     }
 
 }
-
--- ex1/src/com/company/Main.java
+++ ex1/src/com/company/Main.java
@@ -1,6 +1,6 @@
 /**************************************
- * Author: Noam Solan
- * ID: 204484703
+ * Authors: Noam Solan & Ronen Rozin
+ * ID: 204484703 (Noam) & 318257011 (Ronen)
  * Course: 83-459-01 (Software engineering)
  * Practice: 83-459-03 (Gil)
  **************************************/
